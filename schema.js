010840b300105243a54879cf965dc24b3f039907
Grant Mathews <grant@causes.com>
2011-10-30 16:44:57 -0700

    Fix schema so that it actually terminates.
    Well, it's computationally impossible to say that it actually terminates
    in all circumstances, but that's not important right now.

--- schema.js
+++ schema.js
@@ -1,8 +1,9 @@
-(function() {
-  var con_string, db, pg;
-  pg = require('pg');
-  con_string = process.env.DATABASE_URL || "tcp://grant:a@localhost:5432/bestdb";
-  db = new pg.Client(con_string);
-  db.connect();
-  db.query("create table posts (content varchar(10000), author varchar(20))");
-}).call(this);
+var pg = require('pg')
+  , connectionString = process.env.DATABASE_URL ||  "tcp://grant:a@localhost:5432/bestdb"
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+query = client.query("create table posts (content varchar(10000), author varchar(20))");
+query.on('end', function() { client.end(); });
