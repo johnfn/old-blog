e39e726f44319583dcc90860c78981d90c61fbb8
Grant Mathews <grant@causes.com>
2011-10-30 16:49:46 -0700

    Heroku appears to be running this file?!

--- schema.js
+++
@@ -1,9 +0,0 @@
-var pg = require('pg')
-  , connectionString = process.env.DATABASE_URL ||  "tcp://grant:a@localhost:5432/bestdb"
-  , client
-  , query;
-
-client = new pg.Client(connectionString);
-client.connect();
-query = client.query("create table posts (content varchar(10000), author varchar(20))");
-query.on('end', function() { client.end(); });
