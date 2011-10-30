f0dd380f4cca63d72a4912e9b250e5d3f6bdb774
Grant Mathews <grant@causes.com>
2011-10-30 16:08:52 -0700

    Add schema.
    Heroku doesnt have a coffee executeable? :(

---
+++ schema.coffee
@@ -0,0 +1,7 @@
+pg = require 'pg'
+con_string = DATABASE_URL || "tcp://grant:a@localhost:5432/bestdb"
+
+db = new pg.Client(con_string)
+db.connect()
+
+db.query "create table posts (content varchar(10000), author varchar(20))"
---
+++ schema.js
@@ -0,0 +1,8 @@
+(function() {
+  var con_string, db, pg;
+  pg = require('pg');
+  con_string = DATABASE_URL || "tcp://grant:a@localhost:5432/bestdb";
+  db = new pg.Client(con_string);
+  db.connect();
+  db.query("create table posts (content varchar(10000), author varchar(20))");
+}).call(this);
